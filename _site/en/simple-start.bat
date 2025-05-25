831b6f5ecd6a313d77d68fcb2a31b9d6a201a00a
ethan <879670231@qq.com>
2025-05-25 10:23:22 +0800

    1

--- _site/en/simple-start.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d G:\hubei2023.github.io
-set PATH=G:\Ruby32-x64\bin;%PATH%
-echo Starting Jekyll server...
-bundle exec jekyll serve --host 127.0.0.1 --port 4000
-echo Jekyll server stopped.
-pause 
